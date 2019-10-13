7d544035dc7c0cdf3fe287b8645cd4d3c0a3124f
Peter <peterhebden6@gmail.com>
2019-10-13 17:41:47 +0100

    Basic rendering

---
+++ Projects/Tetris_Training/Assets/Scripts/HumanIO.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TetrisEngine))]
+public class HumanIO : MonoBehaviour
+{
+    TetrisEngine engine;
+
+    [SerializeField] GameObject tileObj;
+
+    private void Start()
+    {
+        // Obtain reference to the game engine
+        engine = gameObject.GetComponent<TetrisEngine>();
+    }
+    private void Update()
+    {
+        // Update the visible tiles
+        UpdateTiles();   
+    }
+
+    void UpdateTiles()
+    {
+        // Destroy any existing tiles
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Instantiate a new tile if it is present in the engine's field array
+        for (int x = 0; x < engine.field.GetLength(0); x++)
+        {
+            for (int y = 0; y < engine.field.GetLength(1); y++)
+            {
+                if (engine.viewField[x, y] == 1)
+                {
+                    GameObject newTile = Instantiate(tileObj, new Vector2(x, y), Quaternion.identity);
+                    newTile.transform.parent = transform;
+                }
+            }
+        }
+    }
+}
---
+++ Projects/Tetris_Training/Assets/Scripts/TetrisEngine.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisEngine : MonoBehaviour
+{
+    public int[,] field = new int[10, 20];
+    public int[,] viewField = new int[10, 20];
+    int iteration = 0;
+
+    private void Start()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                viewField[x, y] = Slicer3D(Tetrominoes.lTetromino, 0)[x, y];
+            }
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    viewField[x, y] = Slicer3D(Tetrominoes.zTetromino, iteration % 4)[x, y];
+                }
+            }
+            iteration++;
+        }
+    }
+
+    int[,] Slicer3D (int[,,] toSlice, int dimension)
+    {
+        int[,] slice = new int[4, 4];
+
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                slice[x, y] = toSlice[dimension, x, y];
+            }
+        }
+
+        return slice;
+    }
+}
