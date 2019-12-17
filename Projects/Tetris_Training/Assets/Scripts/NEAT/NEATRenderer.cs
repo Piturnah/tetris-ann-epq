b67b6a12bd10ebbf1fa305a15af9ab59dc50ba05
Peter <peterhebden6@gmail.com>
2019-12-17 16:39:39 +0000

    Genome Renderer

---
+++ Projects/Tetris_Training/Assets/Scripts/NEAT/NEATRenderer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NEATRenderer : MonoBehaviour
+{
+    public static void DrawGenome(Genome genome, Vector3 position, Vector2 dimensions, float nodeSize = 0.5f) {
+
+        List<NodeGene> sensors = new List<NodeGene>();
+        List<NodeGene> outputs = new List<NodeGene>();
+        List<NodeGene> hiddens = new List<NodeGene>();
+
+        // get quantities of nodes
+        foreach (NodeGene node in genome.GetNodes().Values) {
+
+            switch (node.getType()) {
+                case NodeGene.TYPE.SENSOR:
+                    sensors.Add(node); break;
+                case NodeGene.TYPE.OUTPUT:
+                    outputs.Add(node); break;
+                case NodeGene.TYPE.HIDDEN:
+                    hiddens.Add(node); break;
+                default:
+                    Debug.LogWarning("Invalid type!"); break;
+            }
+        }
+
+        print(sensors.Count);
+        print(outputs.Count);
+        print(hiddens.Count);
+
+        Dictionary<int, GameObject> drawnNodes = new Dictionary<int, GameObject>();
+
+        // draw sensors
+        for (int i = 0; i < sensors.Count; i++) {
+            GameObject newNode = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            newNode.transform.position = new Vector3(position.x - 0.5f * dimensions.x + i * (dimensions.x / (sensors.Count - 1)),
+                                                    position.y - 0.5f * dimensions.y, 0);
+            newNode.transform.localScale = Vector3.one * nodeSize;
+
+            drawnNodes.Add(i + 1, newNode);
+        }
+
+        // draw outputs
+        for (int i = 0; i < outputs.Count; i++) {
+            GameObject newNode = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            if (outputs.Count != 1) {
+                newNode.transform.position = new Vector3(position.x - 0.5f * dimensions.x + i * (dimensions.x / (outputs.Count - 1)),
+                                                        position.y + 0.5f * dimensions.y, 0);
+            } else {
+                newNode.transform.position = new Vector3(position.x, position.y + 0.5f * dimensions.y, 0);
+            }
+            newNode.transform.localScale = Vector3.one * nodeSize;
+
+            drawnNodes.Add(i + sensors.Count + 1, newNode);
+        }
+
+        // draw hidden
+        List<List<NodeGene>> hiddenLayers = new List<List<NodeGene>>();
+        for (int i = 0; i < hiddens.Count; i++) {
+            GameObject newNode = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            newNode.transform.position = new Vector3(position.x, position.y - 0.5f * dimensions.y + (i + 1) * (dimensions.y / (outputs.Count + 2)), 0);
+
+            newNode.transform.localScale = Vector3.one * nodeSize;
+            drawnNodes.Add(i + sensors.Count + outputs.Count + 1, newNode);
+        }
+
+        // draw direct connections
+        foreach (ConnectionGene connection in genome.GetConnections().Values) {
+
+            if (true) {
+                if (true) {
+                    if (connection.getExpressed()) {
+                        GameObject newConnection = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                        newConnection.transform.position = (drawnNodes[connection.getInNode()].transform.position + drawnNodes[connection.getOutNode()].transform.position) / 2f;
+
+                        newConnection.transform.localScale = new Vector3 (1, Vector3.Distance(drawnNodes[connection.getInNode()].transform.position, drawnNodes[connection.getOutNode()].transform.position), 1);
+                        newConnection.transform.localScale = new Vector3 (newConnection.transform.localScale.y * 0.01f, newConnection.transform.localScale.y, 1);
+
+                        newConnection.transform.rotation = Quaternion.Euler(0,0,90-(Mathf.Atan2(drawnNodes[connection.getOutNode()].transform.position.y - drawnNodes[connection.getInNode()].transform.position.y,
+                                                                                                drawnNodes[connection.getOutNode()].transform.position.x - drawnNodes[connection.getInNode()].transform.position.x )) * Mathf.Rad2Deg * -1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Projects/Tetris_Training/Assets/Scripts/NEAT/NEATTester.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NEATTester : MonoBehaviour
+{
+    Genome firstParent = new Genome();
+    Genome secondParent = new Genome();
+
+    void Start()
+    {
+        // FIRST PARENT
+        firstParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 1));
+        firstParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 2));
+        firstParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 3));
+
+        firstParent.AddNodeGene(new NodeGene(NodeGene.TYPE.OUTPUT, 4));
+        firstParent.AddNodeGene(new NodeGene(NodeGene.TYPE.HIDDEN, 5));
+
+        // CONNECTIONS
+
+        firstParent.AddConnectionGene(new ConnectionGene(1, 4, 1f, true, 1));
+        firstParent.AddConnectionGene(new ConnectionGene(2, 4, 1f, false, 2));
+        firstParent.AddConnectionGene(new ConnectionGene(3, 4, 1f, true, 3));
+        firstParent.AddConnectionGene(new ConnectionGene(2, 5, 1f, true, 4));
+        firstParent.AddConnectionGene(new ConnectionGene(5, 4, 1f, true, 5));
+        firstParent.AddConnectionGene(new ConnectionGene(1, 5, 1f, true, 8));
+
+        // SECOND PARENT
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 1));
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 2));
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.SENSOR, 3));
+
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.OUTPUT, 4));
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.HIDDEN, 5));
+        secondParent.AddNodeGene(new NodeGene(NodeGene.TYPE.HIDDEN, 6));
+
+        // CONNECTIONS
+
+        secondParent.AddConnectionGene(new ConnectionGene(1, 4, 1f, true, 1));
+        secondParent.AddConnectionGene(new ConnectionGene(2, 4, 1f, false, 2));
+        secondParent.AddConnectionGene(new ConnectionGene(3, 4, 1f, true, 3));
+        secondParent.AddConnectionGene(new ConnectionGene(2, 5, 1f, true, 4));
+        secondParent.AddConnectionGene(new ConnectionGene(5, 4, 1f, false, 5));
+
+        secondParent.AddConnectionGene(new ConnectionGene(5, 6, 1f, true, 6));
+        secondParent.AddConnectionGene(new ConnectionGene(6, 4, 1f, true, 7));
+        secondParent.AddConnectionGene(new ConnectionGene(3, 5, 1f, true, 9));
+        secondParent.AddConnectionGene(new ConnectionGene(1, 6, 1f, true, 10));
+
+        
+        Genome offspringGenome = Genome.Crossover(secondParent, firstParent);
+        NEATRenderer.DrawGenome(offspringGenome, Vector3.zero, Vector2.one * 5);
+    }
+}
